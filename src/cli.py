cfb7bdbdcf9787607fed6e79ddf64e5f7a7b82cd
Nikola Ratinac <ratinac.nikola@gmail.com>
2024-02-16 19:30:07 +0100

    add args and options to rstk build command

--- src/cli.py
+++ src/cli.py
@@ -6,18 +6,27 @@ def cli():
     pass
 
 
-@cli.command()
-@click.option(
-    "--algorithm",
-    "-a",
-    required=True,
-    type=click.Choice(["content-knn", "colaborative-knn"]),
+@cli.command(
+    help="Builds a recommendation model using the given algorithm on the given dataset"
 )
-@click.option("--data", "-d", required=True)
-def build(algorithm, data):
+@click.argument(
+    "algorithm",
+    type=click.Choice(
+        ["content-knn", "user-cf-knn", "item-cf-knn"],
+    ),
+)
+@click.argument("dataset", type=click.Path(exists=True))
+@click.option(
+    "--model-path",
+    default="model.pkl",
+    help="Path to the file where the model will be serialized",
+    type=click.Path(),
+)
+def build(algorithm, dataset, model_path):
     click.echo("Building recommender system...")
     click.echo("Algorithm: %s" % algorithm)
-    click.echo("Dataset: %s" % data)
+    click.echo("Dataset: %s" % dataset)
+    click.echo("Model path : %s" % model_path)
 
 
 def preprocess():

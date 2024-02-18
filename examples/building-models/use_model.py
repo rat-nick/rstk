82291038a47a29c4cf48cefdc9bcefa7db7a62a0
metafizikalac <56120027+rat-nick@users.noreply.github.com>
2024-02-18 15:30:06 +0100

    Update use_model.py
    deserialize method no longer needs the cls parameter.

--- examples/building-models/use_model.py
+++ examples/building-models/use_model.py
@@ -1,6 +1,6 @@
 from rstk.algo.content_based.knn import KNN
 
 # load the model
-model = KNN.deserialize(KNN, "model.pkl")
+model = KNN.deserialize("model.pkl")
 
 print(model.get_similar_items([1, 2], k=3))

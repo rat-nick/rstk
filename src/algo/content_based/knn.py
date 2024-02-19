442933e42a64aa271b1113ecb9039e6a39955a2e
Nikola Ratinac <ratinac.nikola@gmail.com>
2024-02-19 20:22:36 +0100

    modified way of requesting recommendations

--- src/algo/content_based/knn.py
+++ src/algo/content_based/knn.py
@@ -1,6 +1,6 @@
 import os
 import pickle
-from typing import List
+from typing import Dict, List
 
 import numpy as np
 import pandas as pd
@@ -96,35 +96,70 @@ class KNN:
                 self.raw2innerID[id] = innerID
                 self.inner2rawID[innerID] = id
 
+    def get_most_similar(self, vector: np.array):
+        """
+        Calculate the most similar items to the input vector in the feature space.
+
+        Parameters:
+            vector (np.array): The feature vector for which to find the most similar items.
+            k (int): The number of most similar items to return.
+
+        Returns:
+            list: A list of the raw IDs of the most similar items to the input vector.
+        """
+
+        similarity = cosine_similarity(vector.reshape(1, -1), self.features)
+        indicies = np.argsort(similarity, axis=1)[0][::-1]
+        return [self.inner2rawID[i] for i in indicies]
+
     def get_recommendations(
         self,
-        user_profile: np.array,
+        profile: np.array = None,
+        ratings: Dict[any, float] = None,
+        preference: List[any] = None,
         k: int = 10,
     ) -> List[any]:
         """
-        Get the k most similar items to the given user profile using cosine similarity.
+        Get recommendations based on the given user profile, user ratings, or user preference.
 
-        Args:
-            user_profile (np.array): The user profile for which to find similar items.
-            k (int): The number of similar items to return. Defaults to 10.
+        Parameters:
+            profile (np.array, optional): The user profile in feature space. Defaults to None.
+            ratings (Dict[any, float], optional): The user ratings as a dictionary mapping raw item IDs to ratings. Defaults to None.
+            preference (List[any], optional): The user preference as a list of raw item IDs. Defaults to None.
+            k (int, optional): The number of recommendations to return. Defaults to 10.
 
         Returns:
-            List[any]: A list of the k most similar items to the user profile.
+            List[any]: A list of raw IDs of recommended items.
+
+        Raises:
+            ValueError: If other than one of profile, ratings, or preference is provided.
         """
-        if user_profile is None:
-            raise ValueError("Must provide the user profile")
+        if sum(p is not None for p in [profile, ratings, preference]) != 1:
+            raise ValueError(
+                "Only one of user_profile, user_ratings, or user_preference can be provided."
+            )
 
-        if type(user_profile) is not np.array:
-            user_profile = np.array(user_profile)
+        vector = None
 
-        # calculate the similarity of all items to the user profile
-        similarity = cosine_similarity(user_profile.reshape(1, -1), self.features)
+        if profile is not None:
+            vector = np.array(profile)
+            return self.get_most_similar(vector)[:k]
 
-        # get the indicies of the k most similar items
-        indicies = np.argsort(similarity, axis=1)[0][::-1]
+        if ratings is not None:
+            raw_ids = list(ratings.keys())
+            inner_ids = [self.raw2innerID[item] for item in ratings.keys()]
+            ratings = np.array(list(ratings.values())).reshape(1, -1)
+            vector = ratings.dot(self.features[inner_ids])
+            most_similar = self.get_most_similar(vector)
+            most_similar = [x for x in most_similar if x not in raw_ids]
+            return most_similar[:k]
 
-        # translate the indicies into raw IDs
-        return [self.inner2rawID[i] for i in indicies][:k]
+        if preference is not None:
+            inner_ids = [self.raw2innerID[item] for item in preference]
+            vector = np.sum(self.features[inner_ids], axis=0)
+            most_similar = self.get_most_similar(vector)
+            most_similar = [x for x in most_similar if x not in preference]
+            return most_similar[:k]
 
     def serialize(self, path: str):
         """

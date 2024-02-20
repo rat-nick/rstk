44ef36254b3c4a747d78d556f3925c7cc8b40549
Nikola Ratinac <ratinac.nikola@gmail.com>
2024-02-20 16:32:29 +0100

    update tests for content-knn

--- test/test_content_knn.py
+++ test/test_content_knn.py
@@ -9,23 +9,28 @@ from ..src.algo.content_based.knn import KNN
 
 @pytest.fixture
 def feature_data():
-    # generate 10 rows and 5 columns of binary data
+    # generate 15 rows of data with 4 columns encoding their IDs in binary
     data = {
-        "id": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-        "ftr_1": [0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
-        "ftr_2": [0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0],
-        "ftr_3": [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-        "ftr_4": [1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0],
-        "ftr_5": [1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1],
+        "id": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+        "ftr_3": [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+        "ftr_2": [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1],
+        "ftr_1": [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1],
+        "ftr_0": [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
     }
     df = pd.DataFrame(data)
     return df
 
 
+@pytest.fixture
+def movie_data():
+    data = pd.read_csv("data/u.item", delimiter="|")
+    df = pd.DataFrame()
+
+
 def test_knn_constructors(feature_data):
     knn = KNN(data=feature_data, id_column="id")
 
-    assert knn.features.shape == (12, 5)
+    assert knn.features.shape == (16, 4)
 
     knn = KNN(
         data=feature_data,
@@ -33,11 +38,11 @@ def test_knn_constructors(feature_data):
         feature_columns=["ftr_1", "ftr_3"],
     )
 
-    assert knn.features.shape == (12, 2)
+    assert knn.features.shape == (16, 2)
 
     knn = KNN(data=feature_data)
 
-    assert knn.features.shape == (12, 5)
+    assert knn.features.shape == (16, 4)
 
     with pytest.raises(ValueError):
         knn = KNN(
@@ -52,22 +57,34 @@ def knn(feature_data):
     return KNN(data=feature_data, id_column="id")
 
 
-def test_get_recommendations(knn):
-    user_profile = np.array([1, 1, 0, 1, 0])
-    recs = knn.get_recommendations(user_profile, 3)
-    assert recs == [9, 6, 4]
+def test_get_most_similar(knn):
+    user_profile = np.array([0, 1, 1, 1])
+    recs = knn.get_most_similar(user_profile)
+    assert recs == [7, 15, 6, 5, 3, 14, 13, 11, 4, 2, 1, 12, 10, 9, 8, 0]
 
 
-def test_get_recommendations_with_list_user_profile(knn):
-    user_profile = [1, 1, 0, 1, 0]
-    recs = knn.get_recommendations(user_profile, 3)
-    assert recs == [9, 6, 4]
-
-
-def test_get_similar_items_with_incorrect_user_profile(knn):
+def test_get_most_similar_items_with_incorrect_user_profile(knn):
     user_profile = ["h", "e", "l", "l", "o"]
     with pytest.raises(ValueError):
-        knn.get_recommendations(user_profile, 3)
+        knn.get_recommendations(user_profile)
+
+
+def test_get_recommendations_with_profile(knn):
+    profile = np.array([0, 1, 1, 1])
+    recs = knn.get_recommendations(profile=profile, k=3)
+    assert recs == [7, 15, 6]
+
+
+def test_get_recommendations_with_ratings(knn):
+    ratings = {6: 1, 1: 1}
+    recs = knn.get_recommendations(ratings=ratings, k=3)
+    recs == [7, 15, 5]
+
+
+def test_get_recommendations_with_preference(knn):
+    preference = [6, 1]
+    recs = knn.get_recommendations(preference=preference, k=3)
+    recs == [7, 15, 5]
 
 
 def test_model_searialization(knn):

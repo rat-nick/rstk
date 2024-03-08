fb1f74070185f2da85b73673afe5e60dadd4e3f9
rat-nick <ratinac.nikola@gmail.com>
2024-03-08 23:40:22 +0100

    add model class

--- src/rstk/model.py
+++ src/rstk/model.py
@@ -1,8 +1,17 @@
+from typing import Callable
+
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+
+
 class Model:
     """
     Base class for all models to be used for recommendation.
     Their sole purpose is to perform a forward pass with given data.
     """
+
+    def __init__(self, data):
+        self.data = data
 
     def forward(self, *args, **kwargs):
         """
@@ -10,3 +19,26 @@ class Model:
         Performs a forward pass with the provided data.
         """
         pass
+
+    @staticmethod
+    def build(*args, **kwargs) -> "Model":
+        pass
+
+
+class KNN(Model):
+    """
+    A class that implements the KNN model.
+    """
+
+    def __init__(
+        self,
+        features,
+        distance_measure: Callable = cosine_similarity,
+    ):
+        super().__init__(features)
+        self.distance_measure = distance_measure
+
+    def forward(self, vector: np.array) -> np.array:
+        distances = self.distance_measure(vector.reshape(1, -1), self.data)
+        indicies = np.argsort(distances, axis=1)[0][::-1]
+        return indicies
